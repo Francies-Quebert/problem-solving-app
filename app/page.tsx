aa3c662573e163e1f4c34d59817c8304005d197f
Francies Fernandes <quebert88@gmail.com>
2024-12-04 23:41:49 +0000

    validating author commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <header className="text-center text-3xl py-4 font-bold">
-        <h5>Problems Solved</h5>
+        <h5>Problems Solved.</h5>
       </header>
       <div>
         <Questions/>
